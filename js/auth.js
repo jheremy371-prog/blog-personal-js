6698b0eb3b06771c16692d117e373eb9cbd348d1
Leonardo <jheremy371@gmail.com>
2026-05-10 21:46:16 -0500

    feat: implementar logica de login y cierre de sesion

--- js/auth.js
+++ js/auth.js
@@ -12,3 +12,23 @@ if (regForm) {
         window.location.href = 'login.html';
     });
 }
+
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+        const user = usuarios.find(u => u.email === email && u.password === password);
+        if (user) {
+            localStorage.setItem('sesionActiva', JSON.stringify(user));
+            window.location.href = 'index.html';
+        } else alert('Credenciales incorrectas');
+    });
+}
+
+function logout() {
+    localStorage.removeItem('sesionActiva');
+    window.location.href = 'login.html';
+}
